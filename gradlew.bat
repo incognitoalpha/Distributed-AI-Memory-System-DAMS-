8fe119075e75cdfc45b6206b5349926b99b9c83c
incognitoalpha <sarthaklakhotia.1550@gmail.com>
2026-05-12 18:47:19 +0530

    Update gradle wrapper properties and batch file

--- gradlew.bat
+++ gradlew.bat
@@ -80,7 +80,8 @@ if %ERRORLEVEL% equ 0 goto mainEnd
 
 :fail
 rem Set variable GRADLE_EXIT_CONSOLE if you need the _script_ return code instead of
-rem having the return code be the _executable_ return code
+rem the _cmd.exe /c_ return code!
+set EXIT_CODE=%ERRORLEVEL%
 if %EXIT_CODE% equ 0 set EXIT_CODE=1
 if not ""=="%GRADLE_EXIT_CONSOLE%" exit %EXIT_CODE%
 exit /b %EXIT_CODE%
